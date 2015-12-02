7dce2cfe8f07836a72effc90ce9240574419c724
luxman Elangeswaralingam <luxman@luxmans-MacBook-Pro.local>
2015-12-02 14:21:25 +0000

    added the missing library

--- app/src/main/java/com/darkphoton/data_visualiser_project/MainActivity.java
+++ app/src/main/java/com/darkphoton/data_visualiser_project/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
