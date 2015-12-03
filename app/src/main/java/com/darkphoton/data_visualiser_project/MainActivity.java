fbdf6d1b6cc2885be0c267a8f6892db704e517fe
Oskar Slyk <o.slyk3@gmail.com>
2015-12-03 10:08:42 +0000

    Added a method for normalizing the percentages for the Education data indicators. There is room for debate with regards to a specific aspect and the usefulness of this data. I've also reintroduced the serialize and deserialize methods that were missing from the development branch as they were introduced, I believe, prior to the creation of the branch.

--- app/src/main/java/com/darkphoton/data_visualiser_project/MainActivity.java
+++ app/src/main/java/com/darkphoton/data_visualiser_project/MainActivity.java
@@ -1,5 +1,7 @@
 package com.darkphoton.data_visualiser_project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,11 @@ import com.darkphoton.data_visualiser_project.data.raw.DataCache;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +48,64 @@ public class MainActivity extends AppCompatActivity {
         txtData = (TextView)findViewById(R.id.txtData);
         JSONDownloader d = new JSONDownloader(this, jsonJob);
         d.execute("http://api.worldbank.org/countries/indicators/5.04.01.02.impexp?per_page=1000&date=2010:2015&format=json");
+
+        /*Temporary test code, for everyone's benefit of understanding how the methods can be used.*/
+        ArrayList testArraylist = new ArrayList();
+        testArraylist.add("Bob");
+        serialize(testArraylist, "testArrayList");
+
+        ArrayList reconstructedArrayList = (ArrayList) deserialize("testArrayList");
+        System.out.println(reconstructedArrayList.get(0));
+        /*End of test code*/
+    }
+
+    /*This method will serialize the given object with the given filename in the /files directory
+    * as a .ser file. You do not need to add any extensions to the filename.*/
+    public void serialize(Object object, String filename){
+        try{
+            Context context = getBaseContext();
+            FileOutputStream fileOutput = context.openFileOutput(filename+".ser", Context.MODE_PRIVATE);
+            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
+            objectOutput.writeObject(object);
+            objectOutput.close();
+            fileOutput.close();
+        }catch (IOException i){
+            i.printStackTrace();
+        }
+    }
+
+    /*This method will deserialize the object with the given filename. You do not need to specify the
+    * file extension, the files are assumed to be .ser and in the /files directory, as if
+    * created with the serialize() method. The deserialized object is returned. */
+    public Object deserialize(String filename){
+        Object object = null;
+        try {
+            FileInputStream fileInput = openFileInput(filename + ".ser");
+            ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+            object = objectInput.readObject();
+            objectInput.close();
+            fileInput.close();
+        }catch (IOException i){
+            i.printStackTrace();
+        }catch (ClassNotFoundException c){
+            System.out.println(filename+" serialized object not found");
+            c.printStackTrace();
+        }
+        return object;
+    }
+
+    /*This method is used for normalizing the percentages from the Education data indicators as all of
+    * them are given in a percentage format. It takes an ArrayList of percentages given as Strings and
+    * returns a single percentage as a String. All but one have been given as "% of GDP per Capita"
+    * except for "Government expenditure on education" which is given as "% of GDP". While this could
+    * be converted to "% of GDP per Capita" it seems somewhat redundant because the data indicators
+     * range dramatically, from 23% to as much as 297%*/
+    public static String normalizePercentages(ArrayList<String> inputArray){
+        int sum = 0;
+        for(String s: inputArray){
+            sum+=Integer.parseInt(s);
+        }
+        return sum/inputArray.size()+"%";
     }
 
     @Override
