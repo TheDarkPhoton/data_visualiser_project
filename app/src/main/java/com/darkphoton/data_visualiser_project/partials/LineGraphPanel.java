a0bc2c867032dd1069e5bca1940c45250649916f
Tharshan Srikaran <Taz@Tharshans-MacBook-Pro.local>
2015-12-10 21:07:43 +0000

    INCOMPLETE line graph

--- app/src/main/java/com/darkphoton/data_visualiser_project/partials/LineGraphPanel.java
+++ app/src/main/java/com/darkphoton/data_visualiser_project/partials/LineGraphPanel.java
@@ -3,13 +3,27 @@ package com.darkphoton.data_visualiser_project.partials;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.os.DropBoxManager;
+import android.util.Log;
 import android.util.Pair;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.darkphoton.data_visualiser_project.MainActivity;
+import com.darkphoton.data_visualiser_project.data.raw.RData;
 import com.darkphoton.data_visualiser_project.data.raw.RIndicator;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -19,17 +33,88 @@ import java.util.ArrayList;
 public class LineGraphPanel extends PartialPanel {
     private Point size = MainActivity.screen_size;
 
+    private LineChart lineChart;
+    private TextView xAxisLabel;
+    private TextView yAxisLabel;
+
+    private ArrayList<Entry> yVals;
+    private ArrayList<String> xVals;
+
+    private String indicName;
+
     public LineGraphPanel(Context context) {
         super(context);
-
-
-
         setLayoutParams(new RelativeLayout.LayoutParams(size.x, size.y));
-        setBackgroundColor(Color.BLUE);
+        setBackgroundColor(Color.WHITE);
         setY(size.y);
+        setGravity(Gravity.CENTER);
+        RelativeLayout relativeLayout= new RelativeLayout(context);
+        relativeLayout.setLayoutParams(
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        addView(relativeLayout);
+
+        xAxisLabel = new TextView(context);
+        xAxisLabel.setText("Year");
+        xAxisLabel.setTextSize(20f);
+        xAxisLabel.setTextColor(Color.BLACK);
+        RelativeLayout.LayoutParams relativeParams1 = new LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        relativeParams1.addRule(Gravity.CENTER);
+        relativeParams1.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+        yAxisLabel = new TextView(context);
+        yAxisLabel.setText(indicName);
+        yAxisLabel.setTextSize(20f);
+        yAxisLabel.setTextColor(Color.BLACK);
+        RelativeLayout.LayoutParams relativeParams2 = new LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        relativeParams2.addRule(RelativeLayout.ALIGN_LEFT, RelativeLayout.TRUE);
+
+        lineChart = new LineChart(context);
+        lineChart.setLayoutParams(
+                new ViewGroup.LayoutParams((int) (size.x * 0.9), (int) (size.y * 0.9)));
+        relativeLayout.addView(lineChart);
+
+        relativeLayout.addView(xAxisLabel, relativeParams1);
+        relativeLayout.addView(yAxisLabel, relativeParams2);
+
+        lineChart.animateXY(7000, 500);
+        lineChart.setDescription("");
+        lineChart.setNoDataTextDescription("No Data Provided");
+        lineChart.setTouchEnabled(false);
+        lineChart.setDragEnabled(false);
+        lineChart.setScaleEnabled(true);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setPinchZoom(true);
+        lineChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.LTGRAY);
+        lineChart.setData(data);
+
+        Legend legend = lineChart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+        legend.setTextColor(Color.LTGRAY);
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setAvoidFirstLastClipping(true);
+
+        YAxis yAxisl = lineChart.getAxisLeft();
+        yAxisl.setTextColor(Color.BLACK);
+        yAxisl.setDrawAxisLine(true);
+        yAxisl.setStartAtZero(false);
+        YAxis yAxisr = lineChart.getAxisRight();
+        yAxisr.setEnabled(false);
     }
 
+
+
+
     public void open(Pair<String, RIndicator> data){
+        setDataValues(data);
+        lineChart.invalidate();
+
         setY(size.y);
         TranslateAnimation animation = new TranslateAnimation(0, 0, 0, -size.y);
         animation.setInterpolator(new DecelerateInterpolator(1.5f));
@@ -46,4 +131,56 @@ public class LineGraphPanel extends PartialPanel {
         animation.setFillAfter(true);
         startAnimation(animation);
     }
+
+    public void setDataValues(Pair<String, RIndicator> rData){
+
+        RIndicator indicator = rData.second;
+        indicName =  "";
+
+        xVals = new ArrayList<>();
+        for (int i = 0; i < indicator.getData().size(); i++) {
+            if(indicator.getData().get(i)!=null) {
+                    xVals.add(indicator.getData().get(i).getDate());
+                    Log.d("test", "" + indicator.getData().get(i).isValid());
+            }
+        }
+
+
+
+        yVals = new ArrayList<>();
+
+        for (int i = 0; i < indicator.getData().size(); i++) {
+            if ( indicator.getData().get(i) != null) {
+                yVals.add(new Entry((float) indicator.getData().get(i).getValue(), i));
+            }
+        }
+
+        LineDataSet set1 = new LineDataSet(yVals, indicName);
+        set1.setDrawCubic(true);
+        set1.setCubicIntensity(0.2f);
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setCircleColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(2f);
+        set1.setCircleSize(3f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(true);
+        set1.setCircleColorHole(Color.WHITE);
+
+        LineData data = new LineData(xVals, set1);
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTextSize(9f);
+        lineChart.setData(data);
+    }
+
+
+
+
+
+
+
+
+
 }
